1f51ec18f7b66ee7e80577b10677250f8e56f524
Vivek Mahakur <vivekmahakur2002@gmail.com>
2025-02-27 16:41:23 +0530

    ..

--- inventory_management/settings.py
+++ inventory_management/settings.py
@@ -70,11 +70,14 @@ WSGI_APPLICATION = 'inventory_management.wsgi.application'
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'neondb',
+        'USER': 'neondb_owner',
+        'PASSWORD': 'npg_jTy1HIUtQK8B',
+        'HOST': 'ep-summer-glade-a48qcnyn-pooler.us-east-1.aws.neon.tech',
+        # 'PORT': '6543',
 }
-
+}
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {
